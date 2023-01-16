6af56188dfc2d117c5908448fae24aaac6b43633
Jay <jaypatel0523@gmail.com>
2023-01-16 11:00:56 +0530

    app updated

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/home', (req, res) => {
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(process.env.PORT || 8000, () => console.log("server is running..."));
+        app.listen(process.env.PORT, () => console.log("server is running..."));
     } catch (error) {
         console.log(error);
     }
